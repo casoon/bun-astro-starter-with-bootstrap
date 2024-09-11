87e46da65b9c1cb5871e341562861589879b212e
casoon <joern.seidel@casoon.de>
2024-09-12 00:07:18 +0200

    sitemap update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 import {defineConfig} from 'astro/config';
 import alpine from '@astrojs/alpinejs';
+import sitemap from '@astrojs/sitemap';
+import config from './src/config'
 
 export default defineConfig({
-    integrations: [alpine()],
+    site: config.siteUrl,
+    integrations: [alpine(), sitemap({
+        filter: (page) =>
+            page !== config.siteUrl + 'test/' &&
+            page !== config.siteUrl + 'test1/',
+        changefreq: 'weekly',
+        priority: 0.7,
+        lastmod: new Date(),
+    })],
     server: {
         port: 3000
     },
